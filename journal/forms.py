37f01b7ff5ba09209fbd64c1c698a675b6b82271
Icy4r <icy4r@virginia.edu>
2023-12-05 18:57:21 -0500

    Made html pretty :)

--- journal/forms.py
+++ journal/forms.py
@@ -3,7 +3,7 @@ from django.contrib.admin.widgets import AdminDateWidget
 
 # forms
 class InteractionForm(forms.Form):
-    your_title = forms.CharField(label='Title', max_length=30)
-    your_convo = forms.CharField(label='Conversation')
+    your_title = forms.CharField(label='Title', max_length=30, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    your_convo = forms.CharField(label='Conversation', widget=forms.Textarea(attrs={'class': 'form-control'}))
     your_date = forms.DateField(label= "Date", widget=forms.DateInput(attrs={'class': 'form-control',
                                                               'type': 'date'}))
